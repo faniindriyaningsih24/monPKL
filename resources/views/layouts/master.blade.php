ec61b6290f5ecc642cba6b7ba147ad102cba8b34
Akmal Adi Saputra <saputraakmal02@gmail.com>
2021-10-02 21:13:42 +0700

    memasukkan logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
   {{-- <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css"> --}}
   <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css') }}">
-
+  <link rel="icon" href="assets/img/monpkl.jpeg" type="image/x-icon">>
   <!-- CSS Libraries -->
 
   <!-- Template CSS -->
