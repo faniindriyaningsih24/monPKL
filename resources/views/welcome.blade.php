7a6be9d30081c72052e4b8628d7e88e0e5df23bb
Akmal Adi Saputra <saputraakmal02@gmail.com>
2021-10-13 16:49:37 +0700

    tes landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,13 @@
         <title>Welcome!</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="icon" src="{{ url('assets/img/monpkl.jpeg') }}" type="image/jpeg">
+        <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css') }}">
+        <link rel="shortcut icon" href="assets/img/monpkl.jpeg" type="image/x-icon">
+        <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/akmal.css') }}">
     </head>
     <body>
-        <div>
+        {{-- <div>
             @if (Route::has('login'))
                 <div>
                     @auth
@@ -23,7 +26,66 @@
                     @endauth
                 </div>
             @endif
-        </div>
-            <h1>ini halaman landing page</h1>
+        </div> --}}
+            {{-- <h1>ini halaman landing page</h1> --}}
+        <nav class="navbar navbar-expand-lg navbar-light bg-purple">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    <div class="login-brand">
+                        <img src="assets/img/monpkl.jpeg" alt="logo" width="60" class="shadow-light rounded-circle">
+                    </div>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+        
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Disabled</a>
+                        </li>
+                    </ul>
+                    <div class="form-inline my-2 my-lg-0">
+                        @auth
+                            <a href="{{ url('/home') }}" class="btn btn-sm btn-info">Home</a>
+                        @else
+                        <a href="{{ route('login') }}" class="bg-login btn btn-sm mr-2 font-monospace">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="bg-regis btn btn-sm ml-2 font-monospace">Register</a>
+                        @endif
+                        @endauth
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+
+
+
+
+
+        <!-- General JS Scripts -->
+        <script src="{{ asset('assets/modules/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/modules/popper.js') }}"></script>
+        <script src="{{ asset('assets/modules/tooltip.js') }}"></script>
+        <script src="{{ asset('assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+        <script src="{{ asset('assets/modules/moment.min.js') }}"></script>
+        <script src="{{ asset('assets/js/stisla.js') }}"></script>
+        
+        <!-- JS Libraies -->
+
+        <!-- Page Specific JS File -->
+        
+        <!-- Template JS File -->
+        <script src="{{ asset('assets/js/scripts.js') }}"></script>
+        <script src="{{ asset('assets/js/custom.js') }}"></script>
     </body>
 </html>
