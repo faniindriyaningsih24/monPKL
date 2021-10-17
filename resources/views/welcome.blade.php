3c0b30108c021636e208c139827918b18613757a
Akmal Adi Saputra <saputraakmal02@gmail.com>
2021-10-17 07:38:39 +0700

    Tambah Card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
             @endif
         </div> --}}
             {{-- <h1>ini halaman landing page</h1> --}}
-        <nav class="navbar navbar-expand-lg navbar-light bg-purple">
+        <nav class="navbar navbar-expand-lg navbar-light bg-purple fixed-top">
             <div class="container">
                 <a class="navbar-brand" href="/">
                     <div class="login-brand">
@@ -43,10 +43,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Home</a>
+                            <a class="nav-link text-white" href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Link</a>
+                            <a class="nav-link text-white" href="#tawarkan">Fitur</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white" href="#">Disabled</a>
@@ -71,7 +71,9 @@
         <div class="container mt-2">
             <div class="row">
                 <div class="col-lg-6 col-sm-12 mt-5">
-                    <div class="alert alert-purple" role="alert">
+                    <div class="mt-5"></div>
+                    <div class="mt-5"></div>
+                    <div class="alert alert-purple mt-2" role="alert">
                         Monitoring Prakerin Anti Ruwet!
                     </div>
                     <h1 class="h1hero">
@@ -87,18 +89,76 @@
                         </div>
 
                         <div class="ml-1">
-                            <a href="#">
+                            <a href="#tawarkan">
                                 <button class="btn btn-lg btn-pelajari mr-2">Pelajari</button><i class='bx bx-search-alt-2 bx-md bx-fade-down' style="color: #00C1D4"></i>
                             </a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-sm-12 mt-5">
-                    <img src="assets/img/undraw2.png" alt="" class="img-fluid">
+                    <img src="assets/img/undraw2.png" alt="" class="img-fluid" style="opacity: .9;">
                 </div>
             </div>
         </div>
 
+        <div id="tawarkan-particles"></div>
+        <div class="container tawarkan">
+            <div class="row mt-5 mb-1">
+                <div class="col-12 text-center mt-5">
+                    <h1 id="tawarkan">Apa yang kami tawarkan?</h1>
+                </div>
+            </div>
+            <div class="row d-flex flex-wrap justify-content-center">
+                <div class="card col-md-4 col-lg-3 col-sm-12 mx-3 my-3 p-3 monpkl-card" style="width: 20rem;">
+                    <img class="card-img-top" height="180px" src="{{ asset('assets/img/company.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Perusahaan</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="/login" class="btn btn-primary">Mulai</a>
+                    </div>
+                </div>
+                <div class="card col-md-4 col-lg-3 col-sm-12 mx-3 my-3 p-3 monpkl-card" style="width: 20rem;">
+                    <img class="card-img-top" height="180px" src="{{ asset('assets/img/teacher.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Guru</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="/login" class="btn btn-primary">Mulai</a>
+                    </div>
+                </div>
+                <div class="card col-md-4 col-lg-3 col-sm-12 mx-3 my-3 p-3 monpkl-card" style="width: 20rem;">
+                    <img class="card-img-top" height="180px" src="{{ asset('assets/img/student.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Siswa</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="/login" class="btn btn-primary">Mulai</a>
+                    </div>
+                </div>
+                <div class="card col-md-4 col-lg-3 col-sm-12 mx-3 my-3 p-3 monpkl-card" style="width: 20rem;">
+                    <img class="card-img-top" height="180px" src="{{ asset('assets/img/mentor.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Pembimbing</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="/login" class="btn btn-primary">Mulai</a>
+                    </div>
+                </div>
+                <div class="card col-md-4 col-lg-3 col-sm-12 mx-3 my-3 p-3 monpkl-card" style="width: 20rem;">
+                    <img class="card-img-top" height="180px" src="{{ asset('assets/img/company-detail.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Detail Perusahaan</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="/login" class="btn btn-primary">Mulai</a>
+                    </div>
+                </div>
+                <div class="card col-md-4 col-lg-3 col-sm-12 mx-3 my-3 p-3 monpkl-card" style="width: 20rem;">
+                    <img class="card-img-top" height="180px" src="{{ asset('assets/img/info-prakerin.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Info Prakerin</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="/login" class="btn btn-primary">Mulai</a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
